daea324d02d9f1e29e0627c0c14dfad2a45d60b6
Ilia Beliaev <schimmerrr@gmail.com>
2026-05-12 09:42:52 +0200

    123

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello, World"}
+@app.get("/queryparameters")
+def query_parameters(param1: str = None, param2: int = None) -> dict:
+
+    
+
+    namen = ["martin", "sophia", "michael", "emma", "maria", "matthias"]
+
+    if not param1:
+        return
+             
+    namen_gefiltert = []
+    for name in namen:
+        if param1 in name:
+            namen_gefiltert.append(name)
+
+    return {
+        "param1": param1,
+        "param2": param2
+
+    }
